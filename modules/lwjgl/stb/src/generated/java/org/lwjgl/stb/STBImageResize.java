af83ccf19d0f576a786e80ea5be251126c74d72a
Curione23 <136803289+Curione23@users.noreply.github.com>
2026-03-27 21:10:32 +0000

    Fix[lwjgl-stb] rewrite nstbir_resize_uint8 for Meteor Client (#4)
    * Fix[lwjgl-stb] rewrite nstbir_resize_uint8
    
    * add javadoc
    
    * remove the bytebuffet version

--- modules/lwjgl/stb/src/generated/java/org/lwjgl/stb/STBImageResize.java
+++ modules/lwjgl/stb/src/generated/java/org/lwjgl/stb/STBImageResize.java
@@ -608,6 +608,20 @@ public class STBImageResize {
     // --- [ stbir_resize_uint8 ] ---
 
     /**
+     * Resizes an image with 8-bit unsigned integer channels.
+     *
+     * @param input_pixels           the pointer to the source image data
+     * @param input_w                the width of the source image in pixels
+     * @param input_h                the height of the source image in pixels
+     * @param input_stride_in_bytes  the distance in bytes from the start of one row to the next in the input. 
+     * Pass 0 if the data is tightly packed.
+     * @param output_pixels          the pointer to the destination buffer
+     * @param output_w               the width of the resized image
+     * @param output_h               the height of the resized image
+     * @param output_stride_in_bytes the distance in bytes from the start of one row to the next in the output. 
+     * Pass 0 if the data is tightly packed.
+     * @param pixel_type             the pixel layout/format (e.g., RGB, RGBA)
+     *
      * @return 1 on success, 0 on failure
      */
     public static int nstbir_resize_uint8(long input_pixels, int input_w, int input_h, int input_stride_in_bytes, long output_pixels, int output_w, int output_h, int output_stride_in_bytes, int pixel_type) {
@@ -616,51 +630,24 @@ public class STBImageResize {
     }
 
     /**
-     * 8-bit version of {@link #stbir_resize_uint8_generic resize_uint8_generic}.
+     * Resizes an image with 8-bit unsigned integer channels.
      *
-     * @param input_pixels           the source image data
-     * @param input_w                the source image width
-     * @param input_h                the source image height
-     * @param input_stride_in_bytes  the offset between successive rows of the source image data in memory, in bytes. You can specify 0 to mean packed continuously in memory
-     * @param output_pixels          returns the scaled image data
-     * @param output_w               the resized image width
-     * @param output_h               the resized image height
-     * @param output_stride_in_bytes the offset between successive rows of the resized image data in memory, in bytes. You can specify 0 to mean packed continuously in memory
-     * @param pixel_type             the pixel layout. One of:<br><table><tr><td>{@link #STBIR_RGB RGB}</td><td>{@link #STBIR_RGBA RGBA}</td><td>{@link #STBIR_4CHANNEL 4CHANNEL}</td><td>...</td></tr></table>
+     * @param input_pixels           the {@link ByteBuffer} containing source image data
+     * @param input_w                the width of the source image
+     * @param input_h                the height of the source image
+     * @param input_stride_in_bytes  the input row stride in bytes (0 for packed)
+     * @param output_pixels          the {@link ByteBuffer} to store the resized image
+     * @param output_w               the width of the output image
+     * @param output_h               the height of the output image
+     * @param output_stride_in_bytes the output row stride in bytes (0 for packed)
+     * @param pixel_type             the pixel layout ({@code stbir_pixel_layout})
      *
-     * @return the output buffer on success, {@code null} on failure
+     * @return true on success, false on failure
      */
-    @NativeType("unsigned char *")
-    public static @Nullable ByteBuffer stbir_resize_uint8(
-        @NativeType("unsigned char const *") ByteBuffer input_pixels, 
-        int input_w, int input_h, int input_stride_in_bytes, 
-        @NativeType("unsigned char *") @Nullable ByteBuffer output_pixels, 
-        int output_w, int output_h, int output_stride_in_bytes, 
-        @NativeType("stbir_pixel_layout") int pixel_type
-    ) {
-        int length = calculateBufferSize(output_w, output_h, output_stride_in_bytes, pixel_type, 1);
-        if (CHECKS) {
-            checkSafe(output_pixels, length);
-        }
-        int result = nstbir_resize_uint8(memAddress(input_pixels), input_w, input_h, input_stride_in_bytes, memAddressSafe(output_pixels), output_w, output_h, output_stride_in_bytes, pixel_type);
-        return result != 0 ? output_pixels : null;
-    }
-
-    /** 8-bit version of {@link #stbir_resize_uint8_generic resize_uint8_generic} with explicit length. */
-    @NativeType("unsigned char *")
-    public static @Nullable ByteBuffer stbir_resize_uint8(
-        @NativeType("unsigned char const *") ByteBuffer input_pixels, 
-        int input_w, int input_h, int input_stride_in_bytes, 
-        @NativeType("unsigned char *") @Nullable ByteBuffer output_pixels, 
-        int output_w, int output_h, int output_stride_in_bytes, 
-        @NativeType("stbir_pixel_layout") int pixel_type, 
-        long length
-    ) {
-        if (CHECKS) {
-            checkSafe(output_pixels, length);
-        }
-        int result = nstbir_resize_uint8(memAddress(input_pixels), input_w, input_h, input_stride_in_bytes, memAddressSafe(output_pixels), output_w, output_h, output_stride_in_bytes, pixel_type);
-        return result != 0 ? output_pixels : null;
+    @NativeType("int")
+    public static boolean stbir_resize_uint8(@NativeType("unsigned char const *") ByteBuffer input_pixels, int input_w, int input_h, int input_stride_in_bytes, @NativeType("unsigned char *") ByteBuffer output_pixels, int output_w, int output_h, int output_stride_in_bytes, int pixel_type) {
+        int result = nstbir_resize_uint8(memAddress(input_pixels), input_w, input_h, input_stride_in_bytes, memAddress(output_pixels), output_w, output_h, output_stride_in_bytes, pixel_type);
+        return result != 0;
     }
 
 }
